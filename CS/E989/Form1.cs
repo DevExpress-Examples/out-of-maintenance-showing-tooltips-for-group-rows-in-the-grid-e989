d860ab6d1fb964584dd803afc6a468010a8d16d3
SvetlanaPN <26325291+SvetlanaPN@users.noreply.github.com>
2019-01-29 17:02:30 +0300

    remove mdb

--- CS/E989/Form1.cs
+++ CS/E989/Form1.cs
@@ -13,30 +13,18 @@ namespace E989 {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+            gridControl1.DataSource = DataHelper.GetData(100);
+        }
+        protected override void OnShown(EventArgs e) {
+            base.OnShown(e);
             GridViewGroupToolTipController groupToolTip = new GridViewGroupToolTipController(gridView1, toolTipController1);
-            groupToolTip.AddColumnGroupRowToolTip(colEmployeeID, EmployeeToolTip);
-            groupToolTip.AddColumnGroupRowToolTip(colCustomerID, CustomerToolTip);
+            var column = gridView1.Columns["ParentID"];
+            column.Group();
+            groupToolTip.AddColumnGroupRowToolTip(column, GetToolTip);
         }
-        void EmployeeToolTip(object value, out string text, out string title) {
-            E989.Data.nwindDataSet.EmployeesRow row = this.nwindDataSet.Employees.FindByEmployeeID((int)value);
-            title = string.Format("<b><color=Blue>{0} {1}</color></b>", row.FirstName, row.LastName);
-            text = string.Format("<b>Birth date</b>: {0}\n<b>Home phone</b>: {1}\n <size=-1>{2}", row.BirthDate, row.HomePhone, row.Notes);
-        }
-        void CustomerToolTip(object value, out string text, out string title) {
-            E989.Data.nwindDataSet.CustomersRow row = this.nwindDataSet.Customers.FindByCustomerID((string)value);
-
-            title = string.Format("<b><color=Green>{0}</color></b>", row.CompanyName);
-            text = string.Format("<b>Contact Name</b>: {0}\n<b>Phone</b>: {1}\n<b>Country</b>: {2}\n<b>City</b>: {3}\n<b>Address</b>: {4}\n", row.ContactName, row.Phone, row.Country, row.City, row.Address);
-        }
-
-        private void Form1_Load(object sender, EventArgs e) {
-            // TODO: This line of code loads data into the 'nwindDataSet.Employees' table. You can move, or remove it, as needed.
-            this.employeesTableAdapter.Fill(this.nwindDataSet.Employees);
-            // TODO: This line of code loads data into the 'nwindDataSet.Customers' table. You can move, or remove it, as needed.
-            this.customersTableAdapter.Fill(this.nwindDataSet.Customers);
-            // TODO: This line of code loads data into the 'nwindDataSet.Orders' table. You can move, or remove it, as needed.
-            this.ordersTableAdapter.Fill(this.nwindDataSet.Orders);
-
+        void GetToolTip(object value, out string title, out string text) {
+            title = string.Format("<b><color=Blue>Group</color></b>");
+            text = string.Format("<b>Value</b>: {0}", value);
         }
 
     }
---
+++ CS/E989/Record.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace E989 {
+    public class Record {
+        public Record() {
+        }
+        int id;
+        public int ID {
+            get { return id; }
+            set {
+                if(id != value) {
+                    id = value;
+                }
+            }
+        }
+        int parentID;
+        public int ParentID {
+            get { return parentID; }
+            set {
+                if(parentID != value) {
+                    parentID = value;
+                }
+            }
+        }
+        string text;
+        public string Text {
+            get { return text; }
+            set {
+                if(text != value) {
+                    text = value;
+                }
+            }
+        }
+        string info;
+        public string Info {
+            get { return info; }
+            set {
+                if(info != value) {
+                    info = value;
+                }
+            }
+        }
+        decimal val;
+        public decimal Value {
+            get { return val; }
+            set {
+                if(val != value) {
+                    val = value;
+                }
+            }
+        }
+        TimeSpan dt;
+        public TimeSpan Dt {
+            get { return dt; }
+            set {
+                if(dt != value) {
+                    dt = value;
+                }
+            }
+        }
+        bool state;
+        public bool State {
+            get { return state; }
+            set {
+                if(state != value) {
+                    state = value;
+                }
+            }
+        }
+        Image image;
+        public Image Image {
+            get { return image; }
+            set {
+                if(image != value) {
+                    image = value;
+                }
+            }
+        }
+    }
+    public class DataHelper {
+        public static BindingList<Record> GetData(int count) {
+            BindingList<Record> records = new BindingList<Record>();
+            for(int i = 0; i < count; i++)
+                records.Add(new Record() {
+                    ID = i,
+                    ParentID = i > 10 ? i % 5 : i % 2,
+                    Text = string.Format("Text{0}", i),
+                    Dt = TimeSpan.FromHours(i),
+                    Value = i % 2 == 0 ? (i * 1.1222213M) : (i * 1.02312M),
+                    State = i % 2 == 0,
+                    Image = SystemIcons.Information.ToBitmap(),
+                    Info = string.Format("Info{0}", i % 2 == 0 ? i : i + 2),
+                });
+            return records;
+        }
+    }
+}
